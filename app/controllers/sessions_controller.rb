9f0001f1acab0b929761572cfef138ae18a71b70
1312801 <1312801@student.hcmus.edu.vn>
2017-10-18 23:43:46 +0700

    Update

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users=User.all
+  end
+  def new
+    @user=User.new
+  end
+  def show
+    @user=User.find(params[:id])
+  end
+  def create
+    @user =User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success]="U created an account successfully!!"
+      redirect_to user_path(current_user)
+    else
+      flash[:error]="Fails!!"
+      render 'new'
+    end
+  end
+  def destroy
+    
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
