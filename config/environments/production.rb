dcb297f51d7c1f148c382f5ad975181f32d30e13
1312801 <1312801@student.hcmus.edu.vn>
2017-10-23 22:10:49 +0700

    Sua file Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'agile-eyrie-65599.herokuapp.com' }
 # ActionMailer Config
 # Setup for production - deliveries, no errors raised
 
