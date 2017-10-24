cc8169f0ea319f7ce59f3231fe68839ee60f40bb
1312801 <1312801@student.hcmus.edu.vn>
2017-10-25 00:33:51 +0700

    Fix facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,'app_id','secret',scope: 'public_profile,email',info_fields: 'id,first_name,middle_name,last_name,email,link'
+  provider :facebook, ENV['app_id'], ENV['secret'],scope: 'email',info_fields: 'id,first_name,email'
 end
